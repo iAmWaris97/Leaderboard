c9f6f7b914c3f29a0c26149523c89dd1a23c2c90
Waris Haleem <43011742+iAmWaris97@users.noreply.github.com>
2022-09-10 01:04:58 +0430

    Add tableData module and update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import './index.css';
+import Score from './modules/competent.js';
+
+const consumeAPI = require('./modules/consumeAPI.js');
+const tableData = require('./modules/tableData.js');
+
+const actualURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
+const myID = '/5sDMmKs383LVojK4Du2a/scores/';
+
+const refresh = document.getElementById('refresh');
+const submit = document.getElementById('submit');
+
+tableData(actualURL, myID);
+
+refresh.addEventListener('click', () => {
+  tableData(actualURL, myID);
+});
+
+submit.addEventListener('click', async () => {
+  const competent = document.querySelector('#name').value;
+  const marks = document.querySelector('#score').value;
+  const competentScore = new Score(competent, marks);
+  await consumeAPI.printScore(actualURL, myID, JSON.stringify(competentScore));
+  tableData(actualURL, myID);
+});
